afc7b47dc942e936f8e30baa95e6438572d9319c
Joyce <joycesantos8900@gmail.com>
2025-01-28 16:20:52 -0300

    add: navbar

---
+++ src/app/components/navBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const navBar = () => {
+  return (
+    <div className="flex gap-2 flex-row mx-2">
+      <div>home</div>
+      <div>about</div>
+      <div>contact</div>
+      <div>cart</div>
+      <div>login</div>
+    </div>
+  )
+}
+
+export default navBar
