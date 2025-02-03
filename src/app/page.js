d79ac7e674348485047634ef51faa1b0eff5b3d1
Joyce <joycesantos8900@gmail.com>
2025-02-03 20:15:04 -0300

    add: logica em navbar, ao está logado.

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,13 @@ import React from 'react'
 import NavBar from './components/navBar'
 import Produtos from './view/produtos'
 import CreateForm from './ui/create-form'
-import Page from "./createProduct/page"
+
 const page = () => {
   return (
     <div>
       <NavBar />
       <Produtos />
-      <CreateForm />
-      <Page/>
+     
     </div>
   )
 }
